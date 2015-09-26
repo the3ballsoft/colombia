2238a113d0f61584167fa2426fb3aacc931d9fc9
jochechavez <joche.echavezm@gmail.com>
2015-09-25 23:23:48 -0500

    update slug field to cities

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Colombia Ruby Hash y JSON
+Colombia Ruby Hash, JSON, SQL
 ========
 
 Contiene un hash en Ruby y un JSON con las ciudades de Colombia filtradas por departamento como llave
+
+## Run reader.py to generate sql file
+pip install slugify 
+python reader.py 
+
+
+#update
+ - Added slug field
--- reader.py
+++ reader.py
@@ -1,14 +1,12 @@
 import json
 from pprint import pprint
+from slugify import slugify
 
 with open('colombia_json.json') as data_file:
     data = json.load(data_file)
 
 id_department = 1
 id_city = 1
-
-#print(data.__class__)
-#print(data['Santander'])
 
 f = open('colombia_sql.sql','w')
 
@@ -22,13 +20,14 @@ for r in data:
     
 f.write('\n/* Table Cities */\n')
 f.write('DROP TABLE IF EXISTS cities;\n')
-f.write('CREATE TABLE IF NOT EXISTS cities( `id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `department_id` int(11) NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`department_id`) REFERENCES departments(`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;;\n\n')
+f.write('CREATE TABLE IF NOT EXISTS cities( `id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `slug` varchar(255) NOT NULL, `department_id` int(11) NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`department_id`) REFERENCES departments(`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;;\n\n')
 id_department = 0
 id_city = 0
 for r in data:
     id_department = id_department+1
     for city in data[r]:
         id_city = id_city+1
-        f.write('INSERT INTO cities(`id`, `name`, `department_id`) VALUES ('+str(id_city)+', "'+city+'", '+str(id_department)+' );\n') # python will convert \n to os.linesep
+        f.write('INSERT INTO cities(`id`, `name`, `slug`, `department_id`) VALUES ('+str(id_city)+', "'+city+'", "'+slugify(city)+'", '+str(id_department)+' );\n') # python will convert \n to os.linesep
 
 f.close() # you can omit in most cases as the destructor will call it
+
